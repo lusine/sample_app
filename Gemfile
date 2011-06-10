2893b36baf4c72f08fb1162921a86be9a0fb5789
Lusine Avetisyan <lusavr@mail.ru>
2011-06-10 23:21:53 +0400

    Finished layout and routes

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'http://rubygems.org'
 
 gem 'rails', '3.0.5'
 gem 'sqlite3'
+gem 'rake', '0.9.1'
 
 group :development do
   gem 'rspec-rails', '2.5.0'
---
+++ config/initializers/mool.rb
@@ -0,0 +1,16 @@
+class String
+  def murulindrome?
+    '12'
+  end
+  def murulindrome2?
+    '12'
+  end
+  def murulindrome3?
+    '12'
+  end
+  def murulindrome4?
+    '12'
+  end
+
+end
+
